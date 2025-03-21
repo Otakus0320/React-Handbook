200ba61fcaf510cc819d9199516b5686c7930ee1
Otakus0320 <jioboyang@gmail.com>
2025-03-21 20:53:25 +0800

    Finished part9 another half of the calendar

--- calendar-component/src/App.tsx
+++ calendar-component/src/App.tsx
@@ -1,9 +1,14 @@
-import Calendar from './Calendar/calendar.tsx';
+import Calendar from './Calendar/Calendar.tsx';
 import dayjs from "dayjs";
+
 function App() {
     return (
         <div className="App">
-            <Calendar value={dayjs('2025-3-20')}></Calendar>
+            <Calendar
+                value={dayjs('2025-3-21')}
+                locale="en-US"
+            >
+            </Calendar>
         </div>
     );
 }
--- calendar-component/src/Calendar/Header.tsx
+++ calendar-component/src/Calendar/Header.tsx
@@ -1,11 +1,33 @@
-const Header = () => {
+import {Dayjs} from "dayjs";
+import {useContext} from "react";
+import LocaleContext from "./LocaleContext.tsx";
+import allLocales from "./locale";
+
+interface HeaderProps {
+    curMonth: Dayjs;
+    preMonthHandler: () => void;
+    nextMonthHandler: () => void;
+    todayHandler: () => void;
+}
+
+const Header = (props: HeaderProps) => {
+    const {
+        curMonth,
+        preMonthHandler,
+        nextMonthHandler,
+        todayHandler
+    } = props;
+
+    const localeContext = useContext(LocaleContext);
+    const CalendarContext = allLocales[localeContext.locale];
+
     return (
         <div className="calendar-header">
             <div className="calendar-header-left">
-                <div className="calendar-header-icon">&lt;</div>
-                <div className="calendar-header-value">2023 年 11 月</div>
-                <div className="calendar-header-icon">&gt;</div>
-                <button className="calendar-header-btn">今天</button>
+                <div className="calendar-header-icon" onClick={preMonthHandler}>&lt;</div>
+                <div className="calendar-header-value">{curMonth.format(CalendarContext.formatMonth)}</div>
+                <div className="calendar-header-icon" onClick={nextMonthHandler}>&gt;</div>
+                <button className="calendar-header-btn" onClick={todayHandler}>{CalendarContext.today}</button>
             </div>
         </div>
     )

e48aa9166effe220256eaa35ccb7fba70a277ea7
Otakus0320 <jioboyang@gmail.com>
2025-04-01 19:38:08 +0800

    Finished learning part26

---
+++ tailwindcss-test/src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className='text-base p-1 border border-black border-solid hover:text-lg md:bg-blue-500'>guang</div>
+    );
+}
+
+export default App;
